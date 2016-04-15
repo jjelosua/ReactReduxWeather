c093178dcd01a69ac206a7a1fdc6b5055718f392
Juan Elosua <juan.elosua@gmail.com>
2016-04-15 11:25:52 +0200

    never change state in reducers always return new instance

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,10 +1,15 @@
 import { FETCH_WEATHER } from '../actions/index';
 // state argument is not application state 
 // only the state this reducer is responsible for
-export default function(state = null, action) {
+export default function(state = [], action) {
     switch(action.type) {
         case FETCH_WEATHER:
-            return action.payload.data;
+            // Never mutate state inside reducers
+            // return state.push(action.payload.data);
+            
+            // return state.concat([action.payload.data]);
+            // Same as above but with ES6 syntax
+            return [ action.payload.data, ...state ]
     }
 
     return state;
